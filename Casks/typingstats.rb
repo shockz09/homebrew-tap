e1d8576b6f295e6ce1c071cde0277bfce01707ca
Rohit Mishra <rohit200827@gmail.com>
2025-12-27 15:37:46 +0530

    Bump typingstats to v1.0.1

--- Casks/typingstats.rb
+++ Casks/typingstats.rb
@@ -1,6 +1,6 @@
 cask "typingstats" do
-  version "1.0.0"
-  sha256 "35828f85fcdf0b561f77d63b157a4ea398fa6dcb1d802f0cac576bce6e94ff7b"
+  version "1.0.1"
+  sha256 "2445ca30879ddb844fe905198012a795380012b26d55bb7a40926369e25934e3"
 
   url "https://github.com/shockz09/TypingStats/releases/download/v#{version}/TypingStats-v#{version}.zip"
   name "TypingStats"
